7fa744395097de750698cf5d97b360b993227f64
BUFFALO4411 <buffaloyt4411@gmail.com>
2025-07-25 23:04:48 +0530

    Add files via upload

--- Python 1.py
+++ Python 1.py
@@ -1,13 +1,20 @@
-num = int(input("Enter te number:"))
-
-if num <=1:
-    print("Nither prime not composite")
-elif num == 2:
-    print("prime number")
+a = 3
+b = 6
+print(a + b)
+print("hello world")
+name = "Buffalo"
+print("hello, " + name)
+name = input("what is your name ?")
+print("nice to meet you, " + name + "!")
+name = input("what is your name?")
+age = input("how old are you?")
+print("Hello, " + name + "! You are " + age + " years old.")
+age = int(input(" How old are you ? "))
+if age >= 18:
+    print("you are an adult !")
 else:
-    for i in range(2,num):
-        if num%i==0:
-            print("Composite Number")
-            break
-        else:
-            print("Prime Number")
+    print("you are not an adult yet chaddi ke andar rehe .")
+count = 1
+while count <= 73:
+    print("count is:", count)
+    count = count + 1
